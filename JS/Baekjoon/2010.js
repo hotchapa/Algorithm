dbde63142bfc8c35118ab5b39d654677e46f4360
jihun <hotchapa@naver.com>
2024-01-24 23:58:10 +0900

    2010 문제풀이

---
+++ JS/Baekjoon/2010.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n")
+const N = input[0];
+let plug = 1;
+for(i=1; i <= N; i++){
+  if(plug){
+    plug -= 1;
+    plug += input[i]
+  }
+}
+console.log(plug);
