012c504cebb9c6d2aa5e87b9d2b458bd4d4b2701
jihun <hotchapa@naver.com>
2024-01-28 22:29:26 +0900

    New : 2018 문제 풀이

---
+++ JS/Baekjoon/2018.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+const filePath = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
+const N = input*1;
+let numList = [];
+let count = 0;
+
+for(let i = 1; i <= N; i++){
+    numList.push(i);
+    while(numList.reduce((acc,cur)=> acc+cur) > N){
+        numList.shift();
+    }
+    if (numList.reduce((acc,cur)=> acc+cur) === N) {
+        count++;
+    }
+}
+
+console.log(count);
