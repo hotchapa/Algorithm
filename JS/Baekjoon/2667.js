ab0f7b97d53336ea37cfb298be04ae4a402b71de
jihun <hotchapa@naver.com>
2024-02-24 16:49:59 +0900

    New : DFS 풀이

--- JS/Baekjoon/2667.js
+++ JS/Baekjoon/2667.js
@@ -1,43 +1,38 @@
-// 1. 주어진 지도 정보를 바탕으로, DFS 이용하여 연결된 집들의 단지를 찾음
-// 2. 각 단지에 속하는 집의 수를 세어 counts에 저장
-// 3. counts를 오름차순으로 정렬, 총 단지 수와 각 단지별 집의 수 출력
-
-const fs = require("fs");
+const fs = require('fs');
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-let input = fs
-  .readFileSync(filePath)
-  .toString()
-  .trim()
-  .split("\n");
-
-const N = Number(input.shift());
-const area = input.map(line => line.split('').map(Number));
-let visited = Array.from({length: N}, () => Array(N).fill(false));
-let counts = [];
-
-// DFS 함수 정의
-function dfs(x, y) {
-    if (y < 0 || y >= N || x < 0 || x >= N || !area[y][x] || visited[y][x]) return;
-    
-    visited[y][x] = true;
-    counts[counts.length -1]++;
-
-    dfs(x - 1, y); // 왼쪽
-    dfs(x + 1, y); // 오른쪽
-    dfs(x, y - 1); // 위쪽
-    dfs(x, y + 1); // 아래쪽
+let input = fs.readFileSync(filePath).toString().split('\n');
+let n = input[0]*1
+let arr = [];
+for(let i = 1; i < n+1; i++){
+    arr.push(input[i].split("").map(x=>+x));
 }
 
-for (let i=0;i<N;i++){
-   for(let j=0;j<N;j++){
-       if(area[i][j]==1 && !visited[i][j]){
-           counts.push(0);
-           dfs(j,i);
-       }
-   }
+let visited = Array.from(Array(n), () => Array(n).fill(0));
+let count = 0;
+let answer = [];
+
+function dfs(arr,x,y,visited){
+    if(x < 0 || x >= n || y < 0 || y >= n || visited[x][y] === 1 || arr[x][y] == 0){
+        return;
+    }
+    visited[x][y] = 1;
+    count++;
+    dfs(arr,x+1,y,visited)
+    dfs(arr,x,y+1,visited)
+    dfs(arr,x-1,y,visited)
+    dfs(arr,x,y-1,visited)
 }
 
-counts.sort((a,b) => a-b);
+for(let i = 0; i<n; i++){
+    for(let j = 0; j<n; j++){
+        if(arr[i][j] == 1 && visited[i][j] !== 1){
+            count = 0;
+            dfs(arr,i,j,visited)
+            answer.push(count);
+        }
+    }
+}
 
-console.log(counts.length);
-counts.forEach(count => console.log(count));
+console.log(answer.length);
+console.log(answer.sort((a, b) => a - b).join('\n'));
+
---
+++ JS/Baekjoon/2668.js
@@ -0,0 +1,54 @@
+// 세로 두 줄, 가로로 N개의 칸으로 이루어진 표
+// 각 칸에는 1이상 N이하인 정수
+// 트리가 아닌것 , 싸이클인 것
+
+// visited: 방문 여부를 저장하는 배열 (N개)
+// checked: 이미 방문했던 노드를 다시 만났는지 확인하는 배열 (N개)
+// answer: 숫자 고르기 결과를 저장하는 배열
+
+// dfs : 현재 노드에서 시작하여 재귀적으로 DFS를 수행하며, 다음 노드는 둘째 줄에 있는 값으로 이동
+// 이미 방문했던 노드를 다시 만나면 순환(싸이클)을 찾았다고 판단하고, 순환 경로에 속한 모든 노드를 결과에 추가
+
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filePath).toString().split("\n");
+const n = input[0] * 1;
+let arr = [0];
+let visited = new Array(n + 1).fill(false);
+let checked = new Array(n + 1).fill(false);
+
+for (let i = 1; i < n + 1; i++) {
+  arr.push(input[i] * 1);
+}
+
+let answer = [];
+
+function dfs(j, arr, visited,answer,checked) {
+  visited[j] = true; // 방문 처리
+  let k = arr[j]; // 다음에 탐색할 노드
+  if (!visited[k]) {
+    dfs(k, arr, visited, answer,checked);
+  } else if (!checked[k]) {
+    // 방문한 적이 있다면???
+    // 안됐으면 싸이클 처리 로직 (answer에다가 값 push)
+    // 순환이 되고 있는 노드 값을 넣어주면 되는데...
+    while (k != j) {
+      answer.push(k);
+      k = arr[k];
+    }
+    answer.push(j)
+  }
+  checked[j] = true;
+}
+
+for (let j = 1; j < n + 1; j++) {
+  if (!visited[j]) {
+    dfs(j, arr, visited, answer,checked);
+  }
+}
+
+console.log(answer.length);
+answer.sort((a, b) => a - b);
+for (let l of answer) {
+  console.log(l);
+}
