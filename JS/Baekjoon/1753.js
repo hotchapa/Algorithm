8161572fa6cc8de4d7298311aa184715ee00708c
jihun <hotchapa@naver.com>
2024-03-23 22:32:54 +0900

    Docs : 문제 링크 수정

--- JS/Baekjoon/1753.js
+++ JS/Baekjoon/1753.js
@@ -1,21 +1,79 @@
 class PriorityQueue {
-  constructor() {
-      this.queue = [];
+    constructor() {
+        this.heap = [];
+    }
+  
+    enqueue(item) {
+        this.heap.push(item);
+        this.bubbleUp();
+    }
+  
+    bubbleUp() {
+        let index = this.heap.length - 1;
+        const element = this.heap[index];
+  
+        while (index > 0) {
+            let parentIndex = Math.floor((index - 1) / 2);
+            let parent = this.heap[parentIndex];
+  
+            if (element[0] >= parent[0]) break;
+  
+            this.heap[index] = parent;
+            this.heap[parentIndex] = element;
+            index = parentIndex;
+        }
+    }
+  
+    dequeue() {
+        const min = this.heap[0];
+        const end = this.heap.pop();
+        if (this.heap.length > 0) {
+            this.heap[0] = end;
+            this.sinkDown();
+        }
+        return min;
+    }
+  
+    sinkDown() {
+        let index = 0;
+        const length = this.heap.length;
+        const element = this.heap[0];
+  
+        while (true) {
+            let leftChildIndex = 2 * index + 1;
+            let rightChildIndex = 2 * index + 2;
+            let leftChild, rightChild;
+            let swap = null;
+  
+            if (leftChildIndex < length) {
+                leftChild = this.heap[leftChildIndex];
+                if (leftChild[0] < element[0]) {
+                    swap = leftChildIndex;
+                }
+            }
+  
+            if (rightChildIndex < length) {
+                rightChild = this.heap[rightChildIndex];
+                if (
+                    (swap === null && rightChild[0] < element[0]) ||
+                    (swap !== null && rightChild[0] < leftChild[0])
+                ) {
+                    swap = rightChildIndex;
+                }
+            }
+  
+            if (swap === null) break;
+  
+            this.heap[index] = this.heap[swap];
+            this.heap[swap] = element;
+            index = swap;
+        }
+    }
+  
+    isEmpty() {
+        return this.heap.length === 0;
+    }
   }
-
-  enqueue(item) {
-      this.queue.push(item);
-      this.queue.sort((a, b) => a[0] - b[0]);
-  }
-
-  dequeue() {
-      return this.queue.shift();
-  }
-
-  isEmpty() {
-      return this.queue.length === 0;
-  }
-}
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 const input = fs.readFileSync(filePath).toString().trim().split("\n");
