53a5babc6bc538270e24f5460d045ad68dcc5636
jihun <hotchapa@naver.com>
2024-04-01 19:40:55 +0900

    New : DFS 풀이

---
+++ JS/Baekjoon/타겟넘버.js
@@ -0,0 +1,12 @@
+function solution(numbers, target) {
+  return dfs(numbers, target, 0, 0);
+}
+
+function dfs(numbers, target, index, sum) {
+  if(index == numbers.length) {
+      return sum == target ? 1 : 0;
+  }
+
+  return dfs(numbers, target, index + 1, sum + numbers[index]) + 
+          dfs(numbers, target, index + 1, sum - numbers[index]);
+}
