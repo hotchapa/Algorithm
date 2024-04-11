137459848df6f77854a0a0170e2581ce21c347ca
jihun <hotchapa@naver.com>
2024-04-11 17:50:49 +0900

    New : DP 풀이

--- JS/Baekjoon/1660.js
+++ JS/Baekjoon/1660.js
@@ -6,17 +6,26 @@ const N = Number(input[0]);
 let dp = new Array(N+1).fill(Infinity); 
 dp[0] = 0; 
 
-let tetrahedrons = []; // 사면체 수를 저장할 배열
-for (let i = 1; ; i++) {
-    let tetrahedron = (i * (i + 1) * (i + 2)) / 6;
-    if (tetrahedron > N) break; // N을 초과하는 경우 반복 종료
-    tetrahedrons.push(tetrahedron);
+let num = 0;
+let index = 1;
+let arr = [];
+
+while(N > num){
+    num += (index*(index+1)/2);
+    arr.push(num);
+    index++;
 }
 
-for (let i = 0; i < tetrahedrons.length; i++) {
-    for (let j = tetrahedrons[i]; j <= N; j++) {
-        dp[j] = Math.min(dp[j], dp[j - tetrahedrons[i]] + 1);
+for (let i = 1; i <= N; i++) {
+    for (const num of arr) {
+        if (num === i) {
+            dp[i] = 1;
+            break;
+        } else if (num > i) {
+            break;
+        }
+        dp[i] = Math.min(dp[i], 1 + dp[i - num]);
     }
 }
 
-console.log(dp[N]); 
+console.log(dp[N]);
