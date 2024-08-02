4ad20d8caa43a746cd2f4b10785759369a96f57c
jihun <hotchapa@naver.com>
2024-08-02 16:57:58 +0900

    New : 코드 수정

--- JS/Baekjoon/가장 많이 받은 선물.js
+++ JS/Baekjoon/가장 많이 받은 선물.js
@@ -1,61 +1,55 @@
 function solution(friends, gifts) {
-  // 각 친구가 받은 선물 횟수와 준 선물 횟수를 저장할 객체 초기화
-  const receivedCount = {};
+  const receiveCount = {};
   const sentCount = {};
-
-  // 친구 목록을 기반으로 초기화
+  
   friends.forEach(friend => {
-      receivedCount[friend] = 0;
       sentCount[friend] = 0;
-  });
-
-  // 선물 기록을 순회하여 선물 횟수 카운트
-  gifts.forEach(gift => {
-      const [from, to] = gift.split(" ");
-      sentCount[from]++;
-      receivedCount[to]++;
-  });
-
-  // 다음 달 예상 선물 수 초기화
-  const predictedGifts = {};
-
-  friends.forEach(friend => {
-      predictedGifts[friend] = 0;
-  });
-
-  // 모든 친구 쌍에 대해 규칙에 따라 선물 예측
-  for (let i = 0; i < friends.length; i++) {
-      for (let j = i + 1; j < friends.length; j++) {
-          const friend1 = friends[i];
-          const friend2 = friends[j];
-
-          const giftsFrom1To2 = gifts.filter(gift => gift === `${friend1} ${friend2}`).length;
-          const giftsFrom2To1 = gifts.filter(gift => gift === `${friend2} ${friend1}`).length;
-
-          if (giftsFrom1To2 > giftsFrom2To1) {
-              predictedGifts[friend1]++;
-          } else if (giftsFrom2To1 > giftsFrom1To2) {
-              predictedGifts[friend2]++;
-          } else {
-              const index1 = sentCount[friend1] - receivedCount[friend1];
-              const index2 = sentCount[friend2] - receivedCount[friend2];
-
-              if (index1 > index2) {
-                  predictedGifts[friend1]++;
-              } else if (index2 > index1) {
-                  predictedGifts[friend2]++;
+      receiveCount[friend] = 0;
+  })
+  
+  gifts.forEach(gift =>{
+      const [from,to] = gift.split(" ");
+      sentCount[from] ++;
+      receiveCount[to] ++;
+  })
+  
+  const presentInfo = {};
+  
+  for(x of friends) {
+      presentInfo[x] = 0;   
+  }
+  
+  for(let i =0; i<friends.length; i++){
+      for(let j =i+1; j<friends.length; j++){
+          // 친구간의 선물 기록 체크를 위한 배열 순회
+          const Aman = friends[i]; 
+          const Bman = friends[j];
+          const AtoB = gifts.filter(gift => gift === `${friends[i]} ${friends[j]}`).length;
+          const BtoA = gifts.filter(gift => gift === `${friends[j]} ${friends[i]}`).length;
+          
+          if(AtoB > BtoA){
+              presentInfo[Aman] ++;
+          }else if(AtoB < BtoA){
+              presentInfo[Bman] ++;
+          }else{ // 두 사람이 선물을 주고받은 기록이 하나도 없거나 주고받은 수가 같다면 선물지수를 비교한다
+              const AmanCount = sentCount[Aman] - receiveCount[Aman];
+              const BmanCount = sentCount[Bman] - receiveCount[Bman];
+              
+              if(AmanCount > BmanCount){
+                presentInfo[Aman] ++;
+              } else if (AmanCount < BmanCount){
+                presentInfo[Bman] ++;
               }
           }
       }
   }
-
-  // 가장 많은 선물을 받을 친구의 수 계산
-  let maxGifts = 0;
-  for (const friend in predictedGifts) {
-      if (predictedGifts[friend] > maxGifts) {
-          maxGifts = predictedGifts[friend];
+  
+  let maxCount = 0;
+  for (const x in presentInfo){
+      if(presentInfo[x] > maxCount){
+          maxCount = presentInfo[x];
       }
   }
-
-  return maxGifts;
+  
+  return maxCount;
 }
